4cab34eb20e0af55e70db9e382e28ddd06a4a93b
samir-web2526 <baishnabsamir26@gmail.com>
2026-02-21 15:00:00 +0600

    review create

--- src/modules/review/review.controller.ts
+++ src/modules/review/review.controller.ts
@@ -1,30 +1,40 @@
 import { Request, Response } from "express";
-import { reviewService } from "./review.service"
+import { reviewService } from "./review.service";
 
-const createReview = async(req:Request,res:Response)=>{
-
+const createReview = async (req: Request, res: Response) => {
+  try {
     const user = req.user;
-    
-    if(!user){
-        res.status(403).json({
-            message:"You ar unauthorized!!"
-        })
+
+    if (!user) {
+      res.status(403).json({
+        message: "You ar unauthorized!!",
+      });
     }
 
-    const result = await reviewService.createReview(req.body,user?.id as string);
+    const result = await reviewService.createReview(
+      req.body,
+      user?.id as string,
+    );
     res.status(201).json({
-        message:"Review Created successfully",
-        data:result
-    })
-}
+      message: "Review Created successfully",
+      data: result,
+    });
+  } catch (error: any) {
+    res.status(404).json({
+      success: false,
+      message: error.message || "Something went wrong",
+    });
+  }
+};
 
-const getReview = async(req:Request,res:Response)=>{
-    const result = await reviewService.getReview();
-    res.status(201).json({
-        message:"Review fetched successfully",
-        data:result
-    })
-}
+const getReview = async (req: Request, res: Response) => {
+  const result = await reviewService.getReview();
+  res.status(201).json({
+    message: "Review fetched successfully",
+    data: result,
+  });
+};
 export const reviewController = {
-    createReview,getReview
-}
+  createReview,
+  getReview,
+};
--- src/modules/review/review.service.ts
+++ src/modules/review/review.service.ts
@@ -2,6 +2,19 @@ import { prisma } from "../../lib/prisma"
 
 const createReview = async(payload:any,userId:string)=>{
 
+    const existBooking = await prisma.booking.findFirst({
+        where:{
+            id:payload.bookingId
+        }
+    })
+    if(!existBooking){
+        throw new Error("Booking not found")
+    }
+
+    if(existBooking.userId !== userId || existBooking.tutorId !== payload.tutorId){
+        throw new Error("You cann't review this tutor because he is not your user");
+    }
+
     const result = await prisma.review.create({
         data:{
             ...payload,userId
