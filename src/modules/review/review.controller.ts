e03521923b81b06199dc3e1bebd30be8492031b5
samir-web2526 <baishnabsamir26@gmail.com>
2026-02-24 01:42:35 +0600

    add route in review and role based access

--- src/modules/review/review.controller.ts
+++ src/modules/review/review.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { reviewService } from "./review.service";
+import { userRole } from "../../middlewares/auth";
 
 const createReview = async (req: Request, res: Response) => {
   try {
@@ -35,26 +36,67 @@ const getReview = async (req: Request, res: Response) => {
   });
 };
 
-const updateReview = async(req: Request, res: Response)=>{
- try {
-   const user = req.user;
-  const reviewId = req.params.reviewId;
-
-  const result = await reviewService.updateReview(req.body,user?.id as string,reviewId as string);
+const getReviewById = async(req: Request, res: Response)=>{
+  try {
+    const reviewId = req.params.reviewId;
+  const result = await reviewService.getReviewById(reviewId as string);
   res.status(201).json({
-    message:"Review updated successfully",
-    data:result,
-  })
-
- } catch (error:any) {
-  res.status(404).json({
+    message: "Review fetched successfully by id",
+    data: result,
+  });
+  } catch (error:any) {
+    res.status(404).json({
       success: false,
       message: error.message || "Something went wrong",
     });
- }
+  }
 }
+
+const updateReview = async (req: Request, res: Response) => {
+  try {
+    const user = req.user;
+    const reviewId = req.params.reviewId;
+
+    const result = await reviewService.updateReview(
+      req.body,
+      user?.id as string,
+      reviewId as string,
+    );
+    res.status(201).json({
+      message: "Review updated successfully",
+      data: result,
+    });
+  } catch (error: any) {
+    res.status(404).json({
+      success: false,
+      message: error.message || "Something went wrong",
+    });
+  }
+};
+
+
+const deleteReview = async(req: Request, res: Response)=>{
+  try {
+    const user = req.user;
+    const reviewId = req.params.reviewId;
+
+    const result = await reviewService.deleteReview(req.body,user?.id as string,reviewId as string,user?.role as userRole);
+    res.status(201).json({
+      message: "Review updated successfully",
+      data: result,
+    });
+  } catch (error:any) {
+     res.status(404).json({
+      success: false,
+      message: error.message || "Something went wrong",
+    });
+  }
+}
+
 export const reviewController = {
   createReview,
   getReview,
-  updateReview
+  updateReview,
+  getReviewById,
+  deleteReview
 };
--- src/modules/review/review.service.ts
+++ src/modules/review/review.service.ts
@@ -1,75 +1,114 @@
-import { prisma } from "../../lib/prisma"
+import { prisma } from "../../lib/prisma";
+import { userRole } from "../../middlewares/auth";
 
-const createReview = async(payload:any,userId:string)=>{
+const createReview = async (payload: any, userId: string) => {
+  const existBooking = await prisma.booking.findFirst({
+    where: {
+      id: payload.bookingId,
+    },
+  });
 
-    const existBooking = await prisma.booking.findFirst({
-        where:{
-            id:payload.bookingId
-        }
-    })
+  const alreadyReviewed = await prisma.review.findUnique({
+    where: {
+      bookingId: payload.bookingId,
+    },
+  });
 
-    const alreadyReviewed = await prisma.review.findUnique({
-        where:{
-            bookingId:payload.bookingId
-        }
-    })
+  if (alreadyReviewed) {
+    throw new Error(
+      "You cann't reviewed again because you already reviewed this session",
+    );
+  }
 
-    if(alreadyReviewed){
-        throw new Error("You cann't reviewed again because you already reviewed this session")
+  if (!existBooking) {
+    throw new Error("Booking not found");
+  }
+
+  if (existBooking.status !== "COMPLETED") {
+    throw new Error(
+      "You cann't review this tutor because session is not completed",
+    );
+  }
+
+  if (
+    existBooking.userId !== userId ||
+    existBooking.tutorId !== payload.tutorId
+  ) {
+    throw new Error("You cann't review this tutor because he is not your user");
+  }
+
+  const result = await prisma.review.create({
+    data: {
+      ...payload,
+      userId,
+    },
+  });
+  return result;
+};
+
+const getReview = async () => {
+  const result = await prisma.review.findMany();
+  return result;
+};
+
+const getReviewById = async(reviewId:string)=>{
+  const result = await prisma.review.findUnique({
+    where:{
+      id:reviewId
     }
-
-    if(!existBooking){
-        throw new Error("Booking not found")
-    }
-
-    if(existBooking.status !== 'COMPLETED'){
-        throw new Error("You cann't review this tutor because session is not completed")
-    }
-
-    if(existBooking.userId !== userId || existBooking.tutorId !== payload.tutorId){
-        throw new Error("You cann't review this tutor because he is not your user");
-    }
-
-    const result = await prisma.review.create({
-        data:{
-            ...payload,userId
-        }
-    })
-    return result;
-   
+  });
+  return result;
 }
 
-const getReview = async()=>{
-    const result = await prisma.review.findMany();
-    return result;
-}
+const updateReview = async (payload: any, userId: string, reviewId: string) => {
+  const existReview = await prisma.review.findUnique({
+    where: {
+      id: reviewId,
+    },
+  });
+  if (!existReview) {
+    throw new Error("You don't reviewed any tutor!!");
+  }
+  if (existReview.userId !== userId) {
+    throw new Error("You are not allowed to update this review");
+  }
+  const updateReview = await prisma.review.update({
+    where: {
+      id: reviewId,
+    },
+    data: {
+      ...payload,
+    },
+  });
+  return updateReview;
+};
 
+const deleteReview = async(payload:any,userId:string,reviewId:string,role:userRole)=>{
 
-const updateReview = async(payload:any,userId:string,reviewId:string)=>{
-    const existReview = await prisma.review.findUnique({
-        where:{
-            id:reviewId
-        }
-    })
-    if(!existReview){
-        throw new Error("You don't reviewed any tutor!!")
+  const existReview = await prisma.review.findUnique({
+    where:{
+      id:reviewId
     }
-    if(existReview.userId !== userId){
-       throw new Error("You are not allowed to update this review");
+  });
+  if(!existReview){
+    throw new Error("Review not found!!");
+  }
+
+  if(role === "STUDENT" && existReview.userId !== userId){
+    throw new Error("You can delete only your own review");
+  }
+  
+  return await prisma.review.delete({
+    where:{
+      id:reviewId
     }
-     const updateReview = await prisma.review.update({
-            where:{
-                id:reviewId
-            },
-            data:{
-                ...payload
-            }
-        });
-        return updateReview
+  })
 }
 
 export const reviewService = {
-    createReview,
-    getReview,
-    updateReview
-}
+  createReview,
+  getReview,
+  updateReview,
+  getReviewById,
+  deleteReview
+};
