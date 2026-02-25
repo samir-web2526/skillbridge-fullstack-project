e63f736390ed6da407e7c2fbafbcbe5e619cf0c0
samir-web2526 <baishnabsamir26@gmail.com>
2026-02-25 11:30:35 +0600

    change ts

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,19 @@
-import app from "./app";
-import { prisma } from "./lib/prisma";
-const PORT = process.env.PORT || 5000
-async function main(){
-    try {
-        await prisma.$connect();
-        console.log("Database connected successfully");
+import app from "./app.js";
+import { prisma } from "./lib/prisma.js";
+const PORT = process.env.PORT || 5000;
+async function main() {
+  try {
+    await prisma.$connect();
+    console.log("Database connected successfully");
 
-        app.listen(PORT,()=>{
-            console.log(`Server is connected on Port ${PORT}`)
-        });
-       
-    } catch (error) {
-         console.log(error);
-         await prisma.$disconnect();
-         process.exit(1);
-    }
+    app.listen(PORT, () => {
+      console.log(`Server is connected on Port ${PORT}`);
+    });
+  } catch (error) {
+    console.log(error);
+    await prisma.$disconnect();
+    process.exit(1);
+  }
 }
 
-main()
+main();
